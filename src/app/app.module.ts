df3ecf40940fcb39d4f75b6ca3b985529ee79ccc
core-santander <pugipedro@gmail.com>
2018-07-11 11:45:50 +0200

    pr v

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { DirectivangforComponent } from './directivangfor/directivangfor.component';
+import { DirectivangifComponent } from './directivangif/directivangif.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DirectivangforComponent,
+    DirectivangifComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
